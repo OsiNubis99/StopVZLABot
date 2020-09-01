2d62862c68cc43bc9dc79dac1281ce7b6014dd0a
Andres Hurrtado <osinubis99@gmail.com>
2020-08-31 21:17:25 -0400

    Express webhook updated

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-// ----------------------- Server -----------------------
+const TOKEN = process.env.TELEGRAM_TOKEN || "YOUR_TELEGRAM_BOT_TOKEN";
+const url = process.env.APP_URL || "https://stopvzlabot.herokuapp.com:443";
+const port = process.env.PORT;
+const is_dev = process.env.IS_DEV;
+
+const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 
-var app = express();
+let options = {};
+if (is_dev == "true") {
+  options = {
+    polling: true,
+  };
+}
+
+const bot = new TelegramBot(TOKEN, options);
+
+if (process.env.IS_DEV != "true") bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+app.use(express.json());
 
 app.use(express.static("./public"));
 
@@ -13,64 +31,66 @@ app.get("/Saludar", (req, res) => {
   res.send("CaidaBot!");
 });
 
-var server = app.listen(process.env.PORT || 3000, () => {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Example app listening at http://%s:%s", host, port);
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
-// ------------------------ Bot ------------------------
-// var bot = require("./configs/bot");
+var server = app.listen(port, function () {
+  console.log(
+    `Express server is listening on ${server.address().address}:${port}`
+  );
+});
 
-// var groups = ["-1001432406771", "-358611014"];
+var groups = ["-1001432406771", "-358611014"];
 
-// bot.on("inline_query", (query) => {
-//   bot.answerInlineQuery(
-//     query.id,
-//     [
-//       {
-//         id: "3",
-//         type: "article",
-//         input_message_content: {
-//           message_text: "/unirse",
-//         },
-//         title:
-//           "No se ha unido a ningun juego \nEste juego solo esta permitido [Aqui](https://t.me/JuegosVZLA)",
-//       },
-//     ],
-//     {
-//       is_personal: true,
-//       cache_time: 1,
-//     }
-//   );
-// });
+bot.on("inline_query", (query) => {
+  bot.answerInlineQuery(
+    query.id,
+    [
+      {
+        id: "3",
+        type: "article",
+        input_message_content: {
+          message_text: "/unirse",
+        },
+        title:
+          "No se ha unido a ningun juego \nEste juego solo esta permitido [Aqui](https://t.me/JuegosVZLA)",
+      },
+    ],
+    {
+      is_personal: true,
+      cache_time: 1,
+    }
+  );
+});
 
-// bot.on("chosen_inline_result", (query) => {
-//   bot.sendMessage(query.from.id, "Holi");
-// });
+bot.on("chosen_inline_result", (query) => {
+  bot.sendMessage(query.from.id, "Holi");
+});
 
-// bot.onText(/\/saluda (.+)/, (msg, match) => {
-//   let chatId = msg.chat.id;
-//   let resp = "Hola @" + msg.from.username + "\nTu mensaje fue: " + match[1];
-//   bot.sendMessage(chatId, resp);
-// });
+bot.onText(/\/saluda (.+)/, (msg, match) => {
+  let chatId = msg.chat.id;
+  let resp = "Hola @" + msg.from.username + "\nTu mensaje fue: " + match[1];
+  bot.sendMessage(chatId, resp);
+});
 
-// bot.onText(/\/start/, (msg, match) => {
-//   let chatId = msg.chat.id;
-//   let resp = "Hola @" + msg.from.username;
-//   bot.sendMessage(chatId, resp);
-// });
+bot.onText(/\/start/, (msg, match) => {
+  let chatId = msg.chat.id;
+  let resp = "Hola @" + msg.from.username;
+  bot.sendMessage(chatId, resp);
+});
 
-// bot.on("text", (msg) => {
-//   let chatId = msg.chat.id;
-//   let resp = "Hola @" + msg.from.username;
-//   bot.sendMessage(chatId, resp);
-// });
+bot.on("text", (msg) => {
+  let chatId = msg.chat.id;
+  let resp = "Hola @" + msg.from.username;
+  bot.sendMessage(chatId, resp);
+});
 
-// bot.on("error", (error) => {
-//   console.error(error);
-// });
+bot.on("error", (error) => {
+  console.error(error);
+});
 
-// bot.on("polling_error", (error) => {
-//   console.error(error);
-// });
+bot.on("polling_error", (error) => {
+  console.error(error);
+});
